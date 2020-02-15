d9f8e9dfebf8e68bb7ca274db92978ceb801d38c
Miguel Menjivar <menjivarmiguel221@gmail.com>
2020-02-15 12:52:00 -0800

    managed to get the blog partially working, it can display blogs, but I cant seem to post to blogs though, I will still need to continue troubleshooting

--- Version02/blog/src/Components/blogForm.js
+++ Version02/blog/src/Components/blogForm.js
@@ -1,28 +1,103 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import axios from 'axios';
 
-// es-6 format/syntax
-const blogForm = (props) => {
-    return (
-        <div class='container'>
-            <h1>Post Blog</h1>
-            <Form autoComplete='off'>
-                <FormGroup>
-                    <Label for="exampleTitle">Blog Title: </Label>
-                    <Input type="text" name="title" id="exampleTitle" placeholder="Title" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleAuthor">Blog Author: </Label>
-                    <Input type="text" name="author" id="author" placeholder="Author" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleText">Blog Content: </Label>
-                    <Input type="textarea" name="text" id="exampleText" placeholder='Content' />
-                </FormGroup>
-                <Button color='primary'>Submit</Button>
-            </Form>
-        </div>
-    )
-};
+class blogForm extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.onChangeBlogTitle = this.onChangeBlogTitle.bind(this);
+        this.onChangeBlogAuthor = this.onChangeBlogAuthor.bind(this);
+        this.onChangeBlogBody = this.onChangeBlogBody.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            blog_title: '',
+            blog_author: '',
+            blog_body: ''
+        }
+    }
+
+    onChangeBlogTitle(e) {
+        this.setState({
+            blog_title: e.target_value
+        });
+    }
+
+    onChangeBlogAuthor(e) {
+        this.setState({
+            blog_author: e.target_value
+        });
+    }
+
+    onChangeBlogBody(e) {
+        this.setState({
+            blog_author: e.target_value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        console.log('FORM SUBMITTED!');
+        console.log(`Title: ${this.state.title}`);
+        console.log(`Author: ${this.state.author}`);
+        console.log(`Blog: ${this.state.body}`);
+
+        const newBlog = {
+            blog_title: this.state.blog_title,
+            blog_author: this.state.blog_author,
+            blog_body: this.state.blog_body
+        };
+
+        axios.post('http://localhost:3000/blog/new', newBlog)
+            .then(res => console.log(res.data));
+
+        this.setState({
+            blog_title: '',
+            blog_author: '',
+            blog_body: ''
+        })
+    }
+
+    render() {
+        return (
+            <div class='container'>
+                <h1>Post Blog</h1>
+                <Form autoComplete='off' onSubmit={this.onSubmit}>
+                    <FormGroup>
+                        <Label for="exampleTitle">Blog Title: </Label>
+                        <Input type="text" 
+                               name="title" 
+                               id="exampleTitle" 
+                               placeholder="Title" 
+                               value={this.state.blog_title} 
+                               onChange={this.onChangeBlogTitle}/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="exampleAuthor">Blog Author: </Label>
+                        <Input type="text" 
+                               name="author" 
+                               id="author" 
+                               placeholder="Author" 
+                               value={this.state.blog_author} 
+                               onChange={this.onChangeBlogAuthor}/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="exampleText">Blog Content: </Label>
+                        <Input type="textarea" 
+                               name="text" 
+                               id="exampleText" 
+                               placeholder='Content' 
+                               value={this.state.blog_body} 
+                               onChange={this.onChangeBlogBody}/>
+                    </FormGroup>
+                    <Button color='primary'>Submit</Button>
+                </Form>
+            </div>
+        )
+    };
+}
 
 export default blogForm;
--- Version02/blog/src/pages/ViewBlog.js
+++ Version02/blog/src/pages/ViewBlog.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
-import bootstrap from 'reactstrap';
+// import bootstrap from 'reactstrap';
+import axios from 'axios';
+// import { Link } from 'react-router-dom';
 
+const Blog = props => (
+    <ul>
+        <li>{props.blog.title}</li>
+        <li>{props.blog.author}</li>
+        <li>{props.blog.body}</li>
+    </ul>
+)
 
 class ViewBlog extends Component {
+    // create a new state with emtpy array for blogs
+    constructor(props) {
+        super(props);
+        this.state = {blogs: []};
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:3000/blog')
+            .then(response => {
+                this.setState({ blogs: response.data });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    blogList() {
+        return this.state.blogs.map((currentBlog, i) => {
+            return <Blog blog={ currentBlog } key={i} />
+        });
+    }
+
     render() {
         return (
             <div class='container'>
                 <h1>View Blogs</h1>
+                { this.blogList() }
             </div>
         );
     }
