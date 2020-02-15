8ae1e40fc66d5635c48bdd8f87103b7282cffd59
Miguel Menjivar <menjivarmiguel221@gmail.com>
2020-02-14 23:40:54 -0800

    made minor change in the server, so far front and backend are not communicating just yet

--- Version02/server.js
+++ Version02/server.js
@@ -5,7 +5,26 @@ const app = express();
 
 // connect DB locally
 connectDB();
+const Blog = require('./models/blog');
+
+// push this to its own routes folder
+app.get('/', (req, res, next) => {
+    res.send('BACKEND IS WORKING');
+});
+
+app.get('/blog', (req, res) => {
+    Blog.find({}, (err, blog) => {
+        if(err){
+            console.log('ERROR! inside serverjs');
+        } else {
+            // send blog information to react front
+        }
+    });
+});
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('Server is Running')
+    console.log('Server is Running');
 });
+
+// var port = normalizePort(process.env.PORT || '9000');
+// app.set('port', port);
